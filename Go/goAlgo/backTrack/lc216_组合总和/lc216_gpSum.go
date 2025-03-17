a955d3c8e911b0613d6f72006e3fb1de1cbe4362
刘梦达 <1528411608@qq.com>
2025-03-17 16:12:47 +0800

    lc216

--- Go/goAlgo/backTrack/lc216_组合总和/lc216_gpSum.go
+++ Go/goAlgo/backTrack/lc216_组合总和/lc216_gpSum.go
@@ -1,1 +1,38 @@
 package main
+
+import "fmt"
+
+var res [][]int
+
+func GroupSum(cur, sum, start, k int, path []int) {
+	if cur > sum {
+		return
+	}
+	if len(path) == k {
+		if cur == sum {
+			res = append(res, append([]int{}, path...))
+			return
+		}
+	}
+
+	for i := start; i <= 9-(k-len(path))+1; i++ {
+		cur += i
+		path = append(path, i)
+		GroupSum(cur, sum, i+1, k, path)
+		cur -= i
+		path = path[:len(path)-1]
+	}
+}
+
+func combine() {
+	// 	cur := 0
+	// 	res = make([][]int, 0)
+	path := make([]int, 0)
+	// nums := []int{1, 2, 3, 4, 5, 6, 7}
+	GroupSum(0, 4, 1, 2, path)
+}
+
+func main() {
+	combine()
+	fmt.Println(res)
+}
