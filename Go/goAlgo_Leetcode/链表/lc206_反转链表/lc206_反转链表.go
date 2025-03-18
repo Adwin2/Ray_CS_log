2db412a63394fb1ff3623b5edc05aa4d8a249e55
Adwin2 <1528411608@qq.com>
2025-03-18 12:47:00 +0800

    chore: optimize goAlgo/ structure

---
+++ Go/goAlgo_Leetcode/链表/lc206_反转链表/lc206_反转链表.go
@@ -0,0 +1,86 @@
+package main
+
+type ListNode struct {
+	val  int
+	next *ListNode
+}
+
+// 2.1 合并有序链表
+func MergeList(pa *ListNode, pb *ListNode) *ListNode {
+	//空节点作为起点
+	ept := &ListNode{}
+	cur := ept
+
+	for pa != nil && pb != nil {
+		if pa.val <= pb.val {
+			if pa.val == pb.val {
+				pb = pb.next
+			}
+			cur.next = pa
+			pa = pa.next
+		} else {
+			cur.next = pb
+			pb = pb.next
+		}
+		cur = cur.next
+	}
+
+	if pa != nil {
+		cur.next = pa
+	} else {
+		cur.next = pb
+	}
+	return ept.next
+}
+
+// 2.2 合并有序链表II
+func MergeListII(pa *ListNode, pb *ListNode) *ListNode {
+	//空节点作为起点
+	ept := &ListNode{}
+	cur := ept
+
+	for pa != nil && pb != nil {
+		if pa.val >= pb.val {
+			if pa.val == pb.val {
+				pb = pb.next
+			}
+			cur.next = pa
+			pa = pa.next
+		} else {
+			cur.next = pb
+			pb = pb.next
+		}
+		cur = cur.next
+	}
+
+	if pa != nil {
+		cur.next = pa
+	} else {
+		cur.next = pb
+	}
+	return reverseList(ept.next)
+}
+
+// 2.3 遍历确定最大值
+func MaxVal(p *ListNode) int {
+	var max int = p.val
+	for p != nil {
+		if p.val > max {
+			max = p.val
+		}
+		p = p.next
+	}
+	return max
+}
+
+// 2.4 翻转链表 空间 O(1) 双指针法
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		tmp := head.next
+		head.next = prev
+		prev = head
+		head = tmp
+	}
+	return prev
+}
