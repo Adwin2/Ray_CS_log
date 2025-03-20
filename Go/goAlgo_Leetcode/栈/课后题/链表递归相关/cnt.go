8c5003f71ac16f495e5977ecac028fcb1cab905f
刘梦达 <1528411608@qq.com>
2025-03-20 11:49:46 +0800

    栈课后题10.

---
+++ Go/goAlgo_Leetcode/栈/课后题/链表递归相关/cnt.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Next *Node
+	val  int
+}
+
+func InitNode(v int) *Node {
+	return &Node{
+		Node: nil,
+		val:  v,
+	}
+}
+
+func Init(a, b, c int) *Node {
+	pa := InitNode(a)
+	pb := InitNode(b)
+	pc := InitNode(c)
+	pa.Next = pb
+	pb.Next = pc
+
+	return pa
+}
+
+func cnt(p *Node) int {
+	if p.Next == nil {
+		return 1
+	}
+
+	return 1 + cnt(p.Next)
+}
+
+func main() {
+	p := Init(1, 2, 3)
+	fmt.Println(cnt(p))
+
+	fmt.Scanln()
+}
