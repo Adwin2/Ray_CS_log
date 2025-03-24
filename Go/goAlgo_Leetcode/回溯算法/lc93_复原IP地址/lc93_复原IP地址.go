de1e04f5acfc2eb24de3540ff65bfc8475357b3e
刘梦达 <1528411608@qq.com>
2025-03-24 14:42:45 +0800

    Update: lc93

--- Go/goAlgo_Leetcode/回溯算法/lc93_复原IP地址/lc93_复原IP地址.go
+++ Go/goAlgo_Leetcode/回溯算法/lc93_复原IP地址/lc93_复原IP地址.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+var res [][]string
+
+// startIndex : 每字符仅遍历一遍(i+1)   pointNum 结束条件
+func backtracks(s string, startIndex, pointNum int) {
+
+}
+
+func combine(s string) {
+	backtracks(s, 0, 0)
+}
+
+func main() {
+	combine("25525511135")
+
+	fmt.Println(res)
+	fmt.Scanln()
+}
