76ef90f0e27f84f951cf55986e2d506619e1efd8
Adwin2 <1528411608@qq.com>
2025-03-27 22:34:34 +0800

    Update lc455

--- Go/goAlgo_Leetcode/贪心算法/lc455_分发饼干/lc455_分发饼干.go
+++ Go/goAlgo_Leetcode/贪心算法/lc455_分发饼干/lc455_分发饼干.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 /*
 455. 分发饼干
 
@@ -7,9 +12,27 @@ package main
 在这里添加题目描述
 
 解题思路:
-在这里添加解题思路
+小尺寸满足小胃口 逐个比较 (大的同理)
 */
 
+func CntContentChildren(req, size []int) int {
+	index := 0
+	for i := 0; i < len(size); i++ {
+		if index < len(req) && size[i] >= req[index] {
+			index++
+		}
+	}
+
+	return index
+}
+
 func main() {
-    // 测试代码
+	req := []int{1, 2}
+	size := []int{1, 2, 3}
+	// sort.Slice(req, func(i, j int) bool {
+	// 	return i > j
+	// })
+	sort.Ints(req)
+	sort.Ints(size)
+	fmt.Println(CntContentChildren(req, size))
 }
