9d303bd8b2409331072e8ab4c17c0f1b07cd23cf
Adwin2 <1528411608@qq.com>
2025-04-13 22:26:43 +0800

    Update reflect

--- Go/trial/reflect/reflect.go
+++ Go/trial/reflect/reflect.go
@@ -6,21 +6,23 @@ import (
 )
 
 type config struct {
-	dir string 
-	dbfileName string 
+	dir        string
+	dbfileName string
 }
 
 func main() {
 	conf := &config{
-		dir: "test",
+		dir:        "test",
 		dbfileName: "dbtest",
 	}
-	val := reflect.ValueOf(conf).Elem() //结构体实例的反射值
+	val := reflect.ValueOf(conf).Elem().FieldByName("dir") //结构体实例的反射值
+	getVal := val.String()
+	fmt.Println(len([]byte(getVal)), getVal)
 
 	fmt.Println(val)
 	// fmt.Println(getField(val, "dir"))
 	// fmt.Println(getField(val, "dbfilename"))
-	fmt.Println(val.FieldByName("dir"))
+	// fmt.Println(val.FieldByName("dir"))
 }
 
 // func getField(val reflect.Value, field string) reflect.Value {
@@ -29,11 +31,11 @@ func main() {
 
 // 	for i := 0;i<typ.NumField();i++ {
 // 		structField := typ.Field(i) // 获取字段元信息
-// 		log.Print(structField)  // OP: {dir main string config:"dir" 0 [0] false} or 
+// 		log.Print(structField)  // OP: {dir main string config:"dir" 0 [0] false} or
 // 		fieldName := structField.Name // 字段名
 // 		log.Printf(fieldName)
 // 		tag := structField.Tag.Get("dir") // 标签值
-		
+
 // 		// 匹配标签字段值
 // 		if tag == field || strings.EqualFold(fieldName, field){
 // 			return val.Field(i) // 返回字段值
